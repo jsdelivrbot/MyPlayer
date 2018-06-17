cbf7df37daf5e132caf32652f70dd8f6a148fad3
GalactisIO <31005483+GalactisIO@users.noreply.github.com>
2018-06-17 06:11:17 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,8 +1,15 @@
 $.get('https://raw.githubusercontent.com/GalactisIO/MyPlayer/master/status.json', function(data){
     $('#initPlayer').remove();
     var init = JSON.parse(data)
-    init[0] ? $('#initPlayer').remove(); : console.error('Site not working! May be underway Technical work')$('#initPlayer').remove();$('player').remove();;
-}).fail(function() {
+    console.warn(init[0])
+    if(init[0]) {
+          $('#initPlayer').remove();
+    } else {
+          console.error('Site not working! May be underway Technical work')
+          $('#initPlayer').remove();
+          $('player').remove();
+    }
+  }).fail(function() {
     console.error('Site not working! May be underway Technical work')
     $('#initPlayer').remove();
     $('player').remove();
